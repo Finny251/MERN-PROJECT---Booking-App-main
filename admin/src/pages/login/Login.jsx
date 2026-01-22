0ebc55e39e1f32a822049ca62124e22970dc874d
Finny251 <116091045+Finny251@users.noreply.github.com>
2026-01-22 23:57:55 +0530

    Update Login.jsx

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -23,7 +23,10 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/login`, user);
+      const res = await await axios.post(
+                            `${process.env.REACT_APP_API_URL}/auth/login`,
+                            credentials
+                           );
       if (res.data.isAdmin) {
         dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
 
